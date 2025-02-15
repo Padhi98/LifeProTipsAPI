de397f7c957666a0164e211c84a71d8fa8e599a2
Padhi98 <padhiankita1@gmail.com>
2025-02-15 19:48:09 +0530

    new html page and better post fetching in API

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 import random
 
 app = FastAPI()
 
-REDDIT_URL = "https://www.reddit.com/r/LifeProTips/top.json?t=day&limit=10"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all domains to access
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+REDDIT_URL = "https://www.reddit.com/r/LifeProTips/top.json?t=all&limit=10"
 
 @app.get("/advice")
 def get_random_advice():
